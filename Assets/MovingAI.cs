8a0647a3ca3af09247ce4d404d4095efc93ce387
Sam Collier <sam.fcollier@gmail.com>
2019-03-30 20:20:20 +0100

    Added Map, Camera Settings and Collision information

---
+++ Assets/MovingAI.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingAI : MonoBehaviour
+{
+    public Vector3 InitialMoveVector;
+    public float Speed;
+
+    private Vector3 CurrentMoveVector;
+    private BoxCollider2D boxCollider;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        CurrentMoveVector = InitialMoveVector;
+        boxCollider = GetComponent<BoxCollider2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        Vector3 Impulse = CurrentMoveVector * Speed * Time.deltaTime;
+        Move(Impulse);
+    }
+
+    private void Move(Vector3 InImpulse)
+    {
+        Vector3 CurrentPosition = transform.position;
+        transform.position = CurrentPosition + InImpulse;
+
+        ContactFilter2D ContactFilter = new ContactFilter2D();
+        Collider2D[] Colliders = new Collider2D[1];
+        boxCollider.OverlapCollider(ContactFilter, Colliders);
+
+        if (Colliders[0] != null && Colliders[0].gameObject != null)
+        {
+            if (Colliders[0].gameObject.CompareTag("StaticObj"))
+            {
+                transform.position = CurrentPosition;
+            }
+        }
+    }
+}
