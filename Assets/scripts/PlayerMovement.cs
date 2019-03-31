def1d12c141f4edc200ea06ad58ed0a2a18981a0
Daniel Törbacka <daniel.torbacka@gmail.com>
2019-03-31 10:53:55 +0200

    WIP: player script

---
+++ Assets/scripts/PlayerMovement.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+    public float speed;
+
+    private Vector2 _moveVelocity;
+    private Rigidbody2D _rigidbody2D;
+
+    // Start is called before the first frame update
+    void Start() {
+        _rigidbody2D = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update() {
+        _moveVelocity = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+    }
+
+    void FixedUpdate() {
+        _rigidbody2D.AddForce(_moveVelocity * speed * Time.fixedDeltaTime);
+    }
+}
